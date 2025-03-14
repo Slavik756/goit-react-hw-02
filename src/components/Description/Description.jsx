1d5c2f5963bd50d37686a735fda2fcfcaa40f473
Pokora Viacheslav <slavikpokora567@gmail.com>
2025-03-15 00:54:41 +0100

    add index file

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,11 +1,13 @@
+import style from "./Description.module.css";
+
 const Description = () => {
     return (
-    <>
-    <h1>Sip Happens Café
+    <div className={style.description}>
+    <h1 className={style.header}>Sip Happens Café
     </h1>
     <p>Please leave your feedback about our service by selecting one of the options below.
     </p>
-    </>
+    </div>
     );
 };
 export default Description;
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,11 +1,13 @@
+import style from "./Options.module.css"
+
 const Options = ({ onGood, onNeutral, onBad, onReset, totalFeedback}) => {
     return (
-    <>
+    <div className={style.list}>
     <button onClick={onGood}>Good</button>
     <button onClick={onNeutral}>Neutral</button>
     <button onClick={onBad}>Bad</button>
     {totalFeedback > 0 && (<button onClick={onReset}>Reset</button>)}
-    </>
+    </div>
     );
 };
 export default Options;
